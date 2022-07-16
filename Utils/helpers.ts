b129c7ad2d512efbaa770b09d795c7e0e36b203a
Byron <byron.miller@gmail.com>
2022-07-16 15:43:28 -0400

    Make use of voice parameters

--- Utils/helpers.ts
+++ Utils/helpers.ts
@@ -1,5 +1,7 @@
 import { CONSTANTS } from './constants'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import { DestroyerType } from './bots'
+import { CounterProp, WorkTaskProp } from '../pages'
 // import { useState, useEffect } from 'react'
 // import textToSpeech, { v1, protos} from '@google-cloud/text-to-speech'
 // import fs from 'fs'
@@ -36,10 +38,35 @@ import { useState, useEffect } from 'react'
 
 export const voice = { speak }
 
-async function speak(text: string, whichVoice = 'NEUTRAL') {
-  var msg = new SpeechSynthesisUtterance()
+type WhichVoiceOptions = {
+  voice?: number,
+  pitch?: number,
+  rate?: number,
+  lang?: string,
+}
+
+const defaultVoiceOptions: WhichVoiceOptions = {
+  lang: 'en',
+  pitch: 1,
+  rate: 1,
+  voice: 0
+}
+async function speak(text: string, whichVoice: WhichVoiceOptions = defaultVoiceOptions) {
+  const msg = new SpeechSynthesisUtterance()
+  const voices = speechSynthesis.getVoices()
+  const voice = voices[whichVoice.voice]
+  msg.voice = voice
+  msg.pitch = whichVoice.pitch
+  msg.rate = whichVoice.rate
+  msg.lang = whichVoice.lang
   msg.text = text
+  // msg.lang = "es-ES"
+  // console.log('get voices: ', speechSynthesis.getVoices())
   window.speechSynthesis.speak(msg)
+
+  // window.speechSynthesis.getVoices().forEach(function (voice) {
+  //   console.log(voice.name, voice.default ? voice.default : '')
+  // })
 }
 
 // export async function getStaticProps(context) {
@@ -106,16 +133,20 @@ async function speak(text: string, whichVoice = 'NEUTRAL') {
 
 
 // Default Robot Commentary Setup
+const femaleDefaultVoice: WhichVoiceOptions = { lang: 'en', pitch: 2, rate: 1.7, voice: 3 }
 export const femaleDefault = {
-  and: async function (text: string, options?: any) {
-    voice.speak(text, 'NEUTRAL')
+  and: async function (text: string, options: WhichVoiceOptions = femaleDefaultVoice) {
+    console.log('femaleDefault voice')
+    voice.speak(text, options)
   },
 }
 
 // // Home Defense Variation of Voice
+const femaleDefensiveVoice: WhichVoiceOptions = { lang: 'en', pitch: 1.5, rate: 1.5, voice: 3 }
 export const femaleDefensive = {
-  speak: async function (text: any, options?: any) {
-    voice.speak(text, 'NEUTRAL')
+  speak: async function (text: any, options: WhichVoiceOptions = femaleDefensiveVoice) {
+    console.log('femaleDefensive voice')
+    voice.speak(text, options)
   },
 }
 
@@ -124,7 +155,7 @@ export const femaleDefensive = {
 //   femaleDefensive
 // }
 
-export const speakerHandler = async (waitTime: number, ttsString: string, whichVoice = 'NEUTRAL') => {
+export const speakerHandler = async (waitTime: number, ttsString: string, whichVoice: WhichVoiceOptions = femaleDefaultVoice) => {
   return new Promise((res, rej) => {
     res(setTimeout(() => {
       voice.speak(ttsString, whichVoice)
@@ -140,6 +171,67 @@ export const speakerHandler = async (waitTime: number, ttsString: string, whichV
     // })
   })
 }
+
+export type ExecutionerStateProps = {
+  workTasks: WorkTaskProp,
+  setWorkTasks: React.Dispatch<React.SetStateAction<WorkTaskProp>>,
+  counters: CounterProp,
+  setCounters: React.Dispatch<React.SetStateAction<CounterProp>>
+}
+
+export const executioner = (array: string[], bot: any, scoreUpdate: string | Function, count: number, state: ExecutionerStateProps) => {
+  let executionCount = count
+
+  function hasKey<O>(obj: O, key: PropertyKey): key is keyof O {
+    return key in obj
+  }
+
+  const command = array[0]
+  if (hasKey(bot, command)) {
+    console.log(this as DestroyerType)
+
+    // const botFunction = bot[this?.command]
+    const botFunction = bot.command
+    console.log('command: ', command)
+    if (command && typeof botFunction === 'function') {
+      // const test = Object.values(botFunction(bot.name, bot.type))
+      console.log('inside command function')
+
+      state.setWorkTasks({ ...state.workTasks, ...{ nextTask: array.length, currentTask: botFunction().description, taskIsComplete: false } })
+
+      speakerHandler(botFunction(bot.name, bot.type).eta, '')
+        .then(() => {
+          let nextArray = array.slice(1)
+          state.setWorkTasks({ ...state.workTasks, ...{ nextTask: nextArray.length } })
+          state.setCounters({ ...state.counters, ...{ progressInterval: state.counters.progressInterval + 1 } })
+          executionCount += 1
+          executioner(nextArray, bot, scoreUpdate, count, state)
+        })
+    } else {
+
+      if (executionCount >= 16) {
+        state.setWorkTasks({ ...state.workTasks, ...{ taskIsComplete: true } })
+        speakerHandler(0, `${bot.name} completed the task set! Standing by!`)
+      }
+
+      if (typeof scoreUpdate === 'function') {
+        scoreUpdate()
+      }
+
+      speakerHandler(3, '')
+        .then(() => {
+          state.setWorkTasks({ ...state.workTasks, ...{ currentTask: `${bot.name} completed all tasks!` } })
+        })
+        .then(() => {
+          if (executionCount <= 15) {
+            speakerHandler(0, 'All Done! And ready for second breakfast, Elevensies and more! Yeah, totally stole that word from Pippin!')
+              .then(() => executionCount = 16)
+          }
+        })
+    }
+  }
+}
+
 
 export function createValidation(stage: number, state: string) {
   let noNameCount = stage
