41aeab0a265f29eee260bdf41befb1aa3ffc4437
dashinja <byron@dashinja.com>
2023-01-23 22:36:37 -0500

    Should only do one file

--- Components/PrimaryButton.tsx/PrimaryButton.tsx
+++ Components/PrimaryButton.tsx/PrimaryButton.tsx
@@ -1,20 +1,20 @@
 export const PrimaryButton = ({
   /**Name Attribute - also used in to define data-test-id */
   name,
-                className = '',
+  className = '',
   ...props
 }) => {
-            /**
-   *     Primary Button Classes
+  /**
+                                   *     Primary Button Classes
    */
-  const primaryButtonClasses = 'text-submit-button-text bg-submit-button'
+                 const primaryButtonClasses = 'text-submit-button-text bg-submit-button'
   return (
     <button
       name={name}
-      className={
+                    className={
         'ml-4 py-2 px-5 rounded-md border border-1 font-semibold' +
         ' ' +
-        className +
+className +
         ' ' +
         primaryButtonClasses
       }
